6072dc19e9c47d307b40c76015cffbcb3c95f459
Lokesh <chigurusettisailokesh@gmail.com>
2026-01-02 14:23:24 +0530

    modify changes5

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://blogosphere-adlr.vercel.app",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
